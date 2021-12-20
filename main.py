277b7df9c24ef9a5f09434619e36d50acc327826
inancalp <inancgunalp@gmail.com>
2021-12-20 01:35:44 +0100

    Bugs are solved, until I find another!

--- main.py
+++ main.py
@@ -23,11 +23,7 @@ gui = color_green + """
     | (3,1) | (3,2) | (3,3) |
     -------------------------
 """ + color_white
-
-
-
-
-
+possible_inputs = ['1,1', '1,2', '1,3', '2,1', '2,2', '2,3', '3,1', '3,2', '3,3']
 s1 = {'1,1', '2,2', '3,3'}
 s2 = {'3,1', '2,2', '1,3'}
 
@@ -46,12 +42,20 @@ x = None
 end = False
 quit = False
 
+
 # -----------------------------------------||||-------------------------------------------------
 
 
 def score():
+    global x
+    global player1_point
+    global player2_point
     print(color_red + " P1:" + str(player1_point) + ", P2:" + str(player2_point) + color_white)
     print('')
+    if not x:
+        print(gui)
+
+
 
 def renew_gui():
     global gui
@@ -65,6 +69,9 @@ def renew_gui():
     -------------------------
     """ + color_white
 
+
+# list checkers MAIN FUNCTIONS
+# ---
 def list_checker_p1():
     global x
     global player1_point
@@ -85,25 +92,27 @@ def list_checker_p1():
         l2_p2.clear()
         l3_p2.clear()
         playtime_count = 0
+
     if l1_p1.count(l1_p1[-1]) == 3:
-        print(  'P1 +1')
+        print('P1 +1')
         mess1()
         score()
 
     elif l2_p1.count(l2_p1[-1]) == 3:
-        print(  'P1 +1')
+        print('P1 +1')
         mess1()
         score()
 
     elif s1.issubset(set(l3_p1)):
-        print(  'P1 +1')
+        print('P1 +1')
         mess1()
         score()
 
     elif s2.issubset(set(l3_p1)):
-        print(  'P1 +1')
+        print('P1 +1')
         mess1()
         score()
+
 
 def list_checker_p2():
     global x
@@ -125,51 +134,61 @@ def list_checker_p2():
         l2_p2.clear()
         l3_p2.clear()
         playtime_count = 0
+
     if l1_p2.count(l1_p2[-1]) == 3:
-        print(  'P2 +1')
+        print('P2 +1')
         mess2()
         score()
 
     elif l2_p2.count(l2_p2[-1]) == 3:
-        print(  'P2 +1')
+        print('P2 +1')
         mess2()
         score()
 
     elif s1.issubset(set(l3_p2)):
-        print(  'P2 +1')
+        print('P2 +1')
         mess2()
         score()
 
     elif s2.issubset(set(l3_p2)):
-        print(  'P2 +1')
+        print('P2 +1')
         mess2()
         score()
+
+
+# ---
 
 def append_lists_p1(i):
     l1_p1.append(i[0])
     l2_p1.append(i[-1])
     l3_p1.append(i)
 
+
 def append_lists_p2(i):
     l1_p2.append(i[0])
     l2_p2.append(i[-1])
     l3_p2.append(i)
 
-def quit_function(param):
+
+def quit_function():
     global quit
-    if param == 'quit':
-        quit = True
+    quit = True
     print('')
     print(color_yellow + '  Thanks for playing!' + color_white)
     print('')
+
+
 def text_yellow(text):
     return color_yellow + text + color_white
+
 
 def text_red(text):
     return color_red + text + color_white
 
+
 def text_bold(text):
     return bold + text + color_white
+
 
 def text_white(text):
     return color_white + text + color_white
@@ -186,61 +205,93 @@ print('')
 
 print(color_yellow + '    GL HF!!' + color_white)
 
-
-
 print(gui)
 
 while x is None:
     while True:
+        end = None
         p1_input = input(text_bold('(P1) Row and Column number: '))
-        if p1_input in gui:
-            append_lists_p1(p1_input)
-            playtime_count += 1
-            gui = gui.replace(p1_input, color_yellow + '"X"' + color_green)
-            print(gui)
-            break
+        if p1_input in possible_inputs:
+            if p1_input in gui:
+                append_lists_p1(p1_input)
+                playtime_count += 1
+                gui = gui.replace(p1_input, color_yellow + '"X"' + color_green)
+                print(gui)
+                break
+            elif p1_input == 'quit':
+                quit_function()
+                break
+            else:
+                print(text_red('Row,Column ->> TAKEN'))
+                continue
         elif p1_input == 'quit':
-            quit_function(p1_input)
+            quit_function()
             break
         else:
-            print(text_red('Row and Column ->> not valid OR already taken!'))
+            print(text_red('Invalid Input'))
             continue
+    if quit:
+        break
     if playtime_count == 9:
         end = True
         print(text_red(text_bold('It\'s a DRAW')))
         score()
-        if end:
-            continue
-    if quit:
-        break
+        l1_p1 = []
+        l2_p1 = []
+        l3_p1 = []
+        l1_p2 = []
+        l2_p2 = []
+        l3_p2 = []
+        playtime_count = 0
+        renew_gui()
+        print(gui)
+    if end:
+        continue
     list_checker_p1()
     if x:
         continue
     while True:
+        end = None
         p2_input = input(text_bold('(P2) Row and Column number: '))
-        if p2_input in gui:
-            append_lists_p2(p2_input)
-            playtime_count += 1
-            gui = gui.replace(p2_input, color_yellow + '"O"' + color_green)
-            print(gui)
-            break
+        if p2_input in possible_inputs:
+            if p2_input in gui:
+                append_lists_p2(p2_input)
+                playtime_count += 1
+                gui = gui.replace(p2_input, color_yellow + '"O"' + color_green)
+                print(gui)
+                break
+            elif p2_input == 'quit':
+                quit_function()
+                break
+            else:
+                print(text_red('Row,Column ->> TAKEN'))
+                continue
         elif p2_input == 'quit':
-            quit_function(p2_input)
+            quit_function()
             break
         else:
-            print(text_red('Row and Column ->> not valid OR already taken!'))
+            print(text_red('Invalid Input'))
             continue
+    if quit:
+        break
     if playtime_count == 9:
         end = True
         print(text_red(text_bold('It\'s a DRAW')))
         score()
+        l1_p1 = []
+        l2_p1 = []
+        l3_p1 = []
+        l1_p2 = []
+        l2_p2 = []
+        l3_p2 = []
+        playtime_count = 0
+        renew_gui()
+        print(gui)
     if end:
         continue
-    if quit:
-        break
     list_checker_p2()
 
 if player1_point == 3:
-    print("P1 WON")
+    print(text_bold("P1 WON"))
 elif player2_point == 3:
-    print("P2 WON!")
+    print(text_bold("P2 WON!"))
